7a2fc455655716fe0c41d56148bdfb54472d042a
Alex Carmichael <carmichael.alexander@gmail.com>
2025-12-02 17:09:29 -0800

    updated drizzle packages required updated config

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,10 +1,14 @@
-import type { Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
+import { config } from "dotenv";
 
+    // Load environment variables from .env.local (or .env if path is not specified)
+config({ path: "../../.env" }); 
+console.log("Database URL:", process.env.DATABASE_URL);
 export default {
   schema: './src/schema/index.ts',
   out: './migrations',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL || '',
+    url: process.env.DATABASE_URL || '',
   },
 } satisfies Config;
