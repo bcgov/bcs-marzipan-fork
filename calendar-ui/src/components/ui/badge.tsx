7289be7cb94ff57eaba55785dfe0afd1bc376069
cgovbc Chris Allen <chris.allen@gov.bc.ca>
2025-12-04 08:42:40 -0800

    feat: add shadcn UI components
    - add combobox, command, and popover components
    - update badge and input components

--- calendar-ui/src/components/ui/badge.tsx
+++ calendar-ui/src/components/ui/badge.tsx
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
         outline: 'text-foreground',
+        selected:
+          'border-transparent bg-inverted-background text-inverted-foreground hover:bg-inverted-background/80',
       },
     },
     defaultVariants: {
