d1e1ecaedbcea98ff7efc8f54a4b6255a5bf3112
cgovbc Chris Allen <chris.allen@gov.bc.ca>
2025-12-04 12:56:07 -0800

    update route navigation
    - update route to new form page from calendarentrieslist
    - add route to home in header logo

--- calendar-ui/src/components/Header/Header.tsx
+++ calendar-ui/src/components/Header/Header.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import logo from '../../assets/Logo.svg';
 import { Avatar, Button, Tooltip } from '@fluentui/react-components';
 import { Alert24Regular } from '@fluentui/react-icons';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="box-border flex h-14 w-full items-center border-b-2 border-[#f4f4f4] px-20 py-2">
-      <img src={logo} alt="Logo" className="mr-4 h-10" />
+      <Link to="/">
+        <img src={logo} alt="Logo" className="mr-4 h-10" />
+      </Link>
 
       <div className="ml-auto flex items-center gap-4">
         <Tooltip content="Notifications" relationship="label">
