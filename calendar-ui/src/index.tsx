6a975ba0133832e2e0fafb633e6a4a7f1fc1ca55
cgovbc Chris Allen <chris.allen@gov.bc.ca>
2025-12-08 23:42:14 -0800

    add tanstack provider to client root

--- calendar-ui/src/index.tsx
+++ calendar-ui/src/index.tsx
@@ -4,13 +4,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { CookiesProvider } from 'react-cookie';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './styles/globals.css';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
-  <BrowserRouter>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
